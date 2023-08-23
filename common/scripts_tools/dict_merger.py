85e668e5e10b27165aa9a82f3ae7452a087413ca
Federico Cirina <effedeso@gmail.com>
2023-08-23 09:58:41 +0100

    try dict_merger

---
+++ common/scripts_tools/dict_merger.py
@@ -0,0 +1,22 @@
+import sys, os
+common_folder = 'C:/Users/Administrator/Desktop/project-search-optimisation/common'
+sys.path.insert(0, common_folder)
+
+import random
+import numpy
+from dict_utils import (load_dictionary_data, load_dictionary_compressed, get_fitness,
+                         add_to_dictionary, save_dictionary_data_compress)
+
+dicts_names = []
+dicts = []
+tot_len = 0
+
+for filename in dicts_names:
+    to_add = load_dictionary_compressed(filename)
+    tot_len+=len(to_add)
+    dicts.append(to_add)
+
+cumulative_dictionary = {k: v for d in dicts for k, v in d.items()}
+cum_len = len(cumulative_dictionary)
+save_dictionary_data_compress(cumulative_dictionary, "cumulative_dict")
+print("The sum of length of dictionaries was %s, the len of the cumulative dictionary is %s" % (tot_len, cum_len))
