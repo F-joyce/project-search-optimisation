b309dc1bb2fba7f700c40581fc432face28cbf21
Federico Cirina <effedeso@gmail.com>
2023-08-23 15:49:40 +0100

    .

--- barriers/common/GA_template.py
+++ barriers/common/GA_template.py
@@ -1,6 +1,6 @@
 import sys, os
 common_barrier_folder = 'C:/Users/Administrator/Desktop/project-search-optimisation/barriers/common'
-sys.working_dir_path.insert(0, common_barrier_folder)
+sys.path.insert(0, common_barrier_folder)
 
 import shutil
 import random
