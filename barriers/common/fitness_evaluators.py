d58e26a1e27e4a4654fa6bebfa8a65965d37fda9
Federico Cirina <effedeso@gmail.com>
2023-08-23 14:45:30 +0100

    fixed bug when adding new fitnesses in fitness eval

--- barriers/common/fitness_evaluators.py
+++ barriers/common/fitness_evaluators.py
@@ -74,7 +74,7 @@ def batch_fitness_simulation(population, max_batch):
     total_fitnesses = []
     to_batch_up = []
     for individual in population:
-        if get_fitness(initial_dictionary, individual) == False:
+        if get_fitness(working_dictionary, individual) == False:
             to_batch_up.append(individual)
     new_fitnesses = []
     new_fitnesses_individuals = to_batch_up.copy()
@@ -90,7 +90,7 @@ def batch_fitness_simulation(population, max_batch):
     print("The new fitnesses put in a list are: ", len(new_fitnesses))
     print("The individual to be calculated were: ", len(new_fitnesses_individuals))
 
-    working_dictionary = add_to_dictionary_from_list(working_dictionary, to_batch_up, new_fitnesses)
+    working_dictionary = add_to_dictionary_from_list(working_dictionary, new_fitnesses_individuals, new_fitnesses)
     if len(working_dictionary) > len(initial_dictionary):
         save = Thread(target=save_dictionary_data_compress, args=(working_dictionary, f"{cwd_path}/{stored_dict_name}"))
         print("Opened Thread to save dictionary")
@@ -101,7 +101,7 @@ def batch_fitness_simulation(population, max_batch):
         print("No new fitnesses to save")
 
     for individual in initial_population:
-        fitness = get_fitness(initial_dictionary, individual)
+        fitness = get_fitness(working_dictionary, individual)
         total_fitnesses.append((fitness,))  # fitness is stored as a tuple
                                             # for DEAP eaSimple requirements
 
