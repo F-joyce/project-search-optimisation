de5051d9e85c02de21f68d352f4690ce972792a3
Federico Cirina <effedeso@gmail.com>
2023-08-23 12:41:31 +0100

    updated supernova

--- barriers/common/supernova.py
+++ barriers/common/supernova.py
@@ -1,52 +1,52 @@
+import warnings
+import os
+import datetime
+import time
+import numpy as np
 
-from ansys.mapdl import core as pymapdl
-#new_path = '/home/ansys/ansys_inc/v201/ansys/bin/ansys201'
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    from ansys.mapdl import core as pymapdl
+    from ansys.mapdl.core import launch_mapdl
+
 new_path = 'C:/Program Files/ANSYS Inc/v182/ansys/bin/winx64/ansys182.exe'
 pymapdl.change_default_ansys_path(new_path)
-import os
-from ansys.mapdl.core import launch_mapdl
-import datetime
-import numpy as np
-import time
 
-print('start'+str(datetime.datetime.now().time()))
+def print_time():
+	return datetime.datetime.now().strftime("%H:%M:%S")
+
+print(f'Started simulation at {print_time()}')
 path = os.getcwd()
+num_processor = 2
 
 mapdl=None
 while mapdl is None:
 	try:
 		time.sleep(5)
 		print(path)
-		mapdl = launch_mapdl(nproc=2,run_location=path,additional_switches="-p ane3flds",mode='corba',override=True)
+		mapdl = launch_mapdl(nproc=num_processor,run_location=path,additional_switches="-p ane3flds",mode='corba',override=True)
 	except Exception as e:
 		print(str(e))
 		pass
 
-
 mapdl.run('finish')
 mapdl.run('/clear,start')
 mapdl.run('/PREP7')
-
 mapdl.run('youngSoil=1.0e10')
 mapdl.run('poiss=0.35')
 mapdl.run('densSoil=2000.0')
-
 mapdl.run('youngBarr=10.0e10')
 mapdl.run('poiss=0.35')
 mapdl.run('densBarr=10000.0')
-
 mapdl.run('height=10.0')
 mapdl.run('width=20.0')
 mapdl.run('msize=0.02')
-
 mapdl.run('dtb=2.0	!distance to barrier')
 mapdl.run('bd=1.0	!barrier depth')
 mapdl.run('bw=0.2	!barrier width')
-
 mapdl.run('nts=1000	!appr height/msize')
 mapdl.run('loadhcycle=50')
 mapdl.run('timestep=9.0e-6 !msize/sqrt(E/rho)')
-
 mapdl.run('forx=1000')
 mapdl.run('fory=1000')
 mapdl.run('!elements')
@@ -66,15 +66,16 @@ mapdl.run('ESIZE,msize,0,')
 mapdl.run('MSHKEY,1')
 mapdl.run('mat,1')
 mapdl.run('amesh,1')
+
 cont=0
 
 Bshape=np.loadtxt('./data.csv')
-for horr in range (0,15):
-	for vert in range (0,30):
+for column in range (0,15):
+	for row in range (0,30):
 		if Bshape[cont]==1:		
-			STR='nsel,s,loc,x,dtb-msize/2+msize*2*'+str(horr)+',dtb+msize/2+msize*2*('+str(horr)+'+1)'
+			STR='nsel,s,loc,x,dtb-msize/2+msize*2*'+str(column)+',dtb+msize/2+msize*2*('+str(column)+'+1)'
 			mapdl.run(STR)
-			STR='nsel,r,loc,y,-(msize/2+msize*2*('+str(vert)+'+1)),-(-msize/2+msize*2*'+str(vert)+')'
+			STR='nsel,r,loc,y,-(msize/2+msize*2*('+str(row)+'+1)),-(-msize/2+msize*2*'+str(row)+')'
 			mapdl.run(STR)		
 			mapdl.run('ESLN,S,1')
 			mapdl.run('MPCHG,2, all')
@@ -97,9 +98,9 @@ mapdl.run('edload,add,fy,,LNODE,etime,epressneg')
 mapdl.run('edstart,0,100000000,')
 mapdl.run('edcts,0,0.9')
 mapdl.run('time,5e-3')
-print('before solve'+str(datetime.datetime.now().time()))
+print(f'Started solving at {print_time()}')
 mapdl.run('solve')
-print('solved'+str(datetime.datetime.now().time()))
+print(f'Solved at {print_time()}')
 mapdl.run('FINISH')
 mapdl.run('/POST26')
 
@@ -109,18 +110,7 @@ mapdl.run('*VSCFUN, MINYD, MIN, UYZ')
 mapdl.run('*VSCFUN, MAXYD, MAX, UYZ')
 print(mapdl.parameters['MINYD'])
 print(mapdl.parameters['MAXYD'])
-#r=mapdl.parameters['minyd']
-#r.min()
+
 with open('min.txt', 'w') as f:
 	print(mapdl.parameters['MINYD'],file=f)
-print('done'+str(datetime.datetime.now().time()))
-
-#mapdl.post26()
-
-#mapdl.nsol(nvar='2',node=str(1820),item='U',comp='Y')
-#mapdl.vget(par='uy1',ir='2',tstrt='0')
-#r=mapdl.parameters['uy1']
-#r.min()
-#with open('min.txt', 'w') as f:#
-#	print(r.min(),file=f)
-#print('done'+str(datetime.datetime.now().time()))
+print(f'Finished simulation at {print_time()}')
