79b0c80619622b5c9a0bd3cf4cf699c93c42f737
Federico C <effedeso@gmail,com>
2023-08-25 02:53:53 -0700

    added missing files to plates common

---
+++ common/plates/customMutations.py
@@ -0,0 +1,30 @@
+import random
+
+
+######################################
+# GA Custom Mutation                 #
+######################################
+# 
+
+
+def mutFlipBitCustom(individual, indpb):
+    """Flip the value of the attributes of the input individual and return the
+    mutant. The *individual* is expected to be a :term:`sequence` and the values of the
+    attributes shall stay valid after the ``not`` operator is called on them.
+    The *indpb* argument is the probability of each attribute to be
+    flipped. This mutation is usually applied on boolean individuals.
+
+    :param individual: Individual to be mutated.
+    :param indpb: Independent probability for each attribute to be flipped.
+    :returns: A tuple of one individual.
+
+    This function uses the :func:`~random.random` function from the python base
+    :mod:`random` module.
+    """
+    for i in range(len(individual)):
+        if random.random() < indpb:
+            individual[i] = type(individual[i])(not individual[i])
+
+    individual[450:455] = [1,1,1,1,1]
+
+    return individual,
